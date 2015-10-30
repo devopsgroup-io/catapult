5913ccdd9793e3d48da54a108a470ae1d5905b17
Seth Reeser <seth.reeser@calciumusa.com>
2015-10-30 14:33:24 -0400

    Optimize cloudflare_cache module.

--- provisioners/redhat/modules/cloudflare_cache.sh
+++ provisioners/redhat/modules/cloudflare_cache.sh
@@ -10,43 +10,14 @@ fi
 IFS=. read -a domain_levels <<< "${domain}"
 
 # determine if cloudflare zone exists
-cloudflare_zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${domain_levels[-2]}.${domain_levels[-1]}" \
+cloudflare_zone=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${domain_levels[-2]}.${domain_levels[-1]}" \
 -H "X-Auth-Email: $(catapult company.cloudflare_email)" \
 -H "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
--H "Content-Type: application/json" \
-| python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"]')
+-H "Content-Type: application/json")
 
-if [ "${cloudflare_zone_id}" = "[]" ]; then
-    # create cloudflare zone
-    echo "cloudflare zone does not exist, creating..."
-    cloudflare_zone=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones" \
-    -H "X-Auth-Email: $(catapult company.cloudflare_email)" \
-    -H "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
-    -H "Content-Type: application/json" \
-    --data "{\"name\":\"${domain_levels[-2]}.${domain_levels[-1]}\",\"jump_start\":false}")
-    if [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["success"]')" == "False" ]; then
-        echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["errors"][0]["message"]'
-    else
-        cloudflare_zone_id=$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"]["id"]')
-        echo "success"
-    fi
-else
-    # get cloudflare zone
-    echo "cloudflare zone exists, retrieving..."
-    cloudflare_zone=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${domain_levels[-2]}.${domain_levels[-1]}" \
-    -H "X-Auth-Email: $(catapult company.cloudflare_email)" \
-    -H "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
-    -H "Content-Type: application/json")
-    if [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["success"]')" == "False" ]; then
-        echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["errors"][0]["message"]'
-    else
-        cloudflare_zone_id=$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"][0]["id"]')
-        echo "success"
-    fi
-fi
-
-# clear cloudflare zone cache, providing zone is valid
-if [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["success"]')" == "True" ]; then
+# clear cloudflare zone cache by zone id, if it exists
+if [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"]')" != "[]" ]; then
+    cloudflare_zone_id=$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"][0]["id"]')
     echo "clearing cloudflare zone (${domain_levels[-2]}.${domain_levels[-1]}) cache"
     cloudflare_zone_cache=$(curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/${cloudflare_zone_id}/purge_cache" \
     -H "X-Auth-Email: $(catapult company.cloudflare_email)" \
@@ -58,6 +29,8 @@ if [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys
     else
         echo "success"
     fi
+else
+    echo "cloudflare zone does not exist"
 fi
 
 touch "/catapult/provisioners/redhat/logs/cloudflare_cache.$(catapult websites.apache.$5.domain).complete"
