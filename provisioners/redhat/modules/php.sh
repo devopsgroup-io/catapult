026af46e2713f9d7b813cd15096ced908e1f1419
Steven Britton <stvnbritton@gmail.com>
2015-12-03 18:22:53 -0500

    Introduce PECL GeoIP extension

--- provisioners/redhat/modules/php.sh
+++ provisioners/redhat/modules/php.sh
@@ -24,8 +24,18 @@ sudo yum install -y php-devel
 sudo pear config-set php_ini /etc/php.ini
 sudo yum install -y gcc
 # These extensions are available from » PECL. They may require external libraries. More PECL extensions exist but they are not documented in the PHP manual yet.
+
+# pecl extension: yaml
 sudo yum install -y libyaml-devel
 echo autodetect | sudo pecl upgrade yaml
+
+# pecl extension: geoip
+sudo yum install -y geoip-devel
+sudo pecl install geoip
+
+# grab GeoLiteCity.dat file
+sudo wget --quiet --read-timeout=10 --tries=5 --output-document=/usr/share/GeoIP/GeoIPCity.dat.gz  http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+sudo gunzip /usr/share/GeoIP/GeoIPCity.dat.gz
 
 # restart httpd for changes to reflect
 systemctl reload httpd.service
