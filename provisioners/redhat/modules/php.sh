f254fc7e921bbb001aaad8a9765395c120b3b359
Seth Reeser <seth.reeser@devopsgroup.io>
2016-01-06 15:51:10 -0500

    Complete geoip pecl php extension installation.

--- provisioners/redhat/modules/php.sh
+++ provisioners/redhat/modules/php.sh
@@ -24,18 +24,23 @@ sudo yum install -y php-devel
 sudo pear config-set php_ini /etc/php.ini
 sudo yum install -y gcc
 # These extensions are available from » PECL. They may require external libraries. More PECL extensions exist but they are not documented in the PHP manual yet.
-
 # pecl extension: yaml
+# https://pecl.php.net/package/yaml
 sudo yum install -y libyaml-devel
 echo autodetect | sudo pecl upgrade yaml
-
 # pecl extension: geoip
+# https://pecl.php.net/package/geoip
 sudo yum install -y geoip-devel
-sudo pecl install geoip
-
-# grab GeoLiteCity.dat file
+# this is a beta release but includes a 3 year gap of bug fixes and new functions
+sudo pecl install geoip-1.1.0
+echo autodetect | sudo pecl upgrade geoip
+# http://dev.maxmind.com/geoip/legacy/geolite/
+sudo wget --quiet --read-timeout=10 --tries=5 --output-document=/usr/share/GeoIP/GeoIP.dat.gz  http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+sudo gunzip --force /usr/share/GeoIP/GeoIP.dat.gz
 sudo wget --quiet --read-timeout=10 --tries=5 --output-document=/usr/share/GeoIP/GeoIPCity.dat.gz  http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-sudo gunzip /usr/share/GeoIP/GeoIPCity.dat.gz
+sudo gunzip --force /usr/share/GeoIP/GeoIPCity.dat.gz
+sudo wget --quiet --read-timeout=10 --tries=5 --output-document=/usr/share/GeoIP/GeoIPASNum.dat.gz  http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
+sudo gunzip --force /usr/share/GeoIP/GeoIPASNum.dat.gz
 
 # restart httpd for changes to reflect
 systemctl reload httpd.service
