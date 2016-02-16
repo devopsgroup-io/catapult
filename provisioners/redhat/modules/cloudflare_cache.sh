17d4d756711c7142297d97d4a9624352d4e1abaf
Seth Reeser <seth.reeser@calciumusa.com>
2016-02-16 14:45:27 -0500

    RedHat: Introduce redundancy around CloudFlare API requests spawned from recent downtime.

--- provisioners/redhat/modules/cloudflare_cache.sh
+++ provisioners/redhat/modules/cloudflare_cache.sh
@@ -16,26 +16,43 @@ for domain in "${domains[@]}"; do
     IFS=. read -a domain_levels <<< "${domain}"
 
     # determine if cloudflare zone exists
-    cloudflare_zone=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${domain_levels[-2]}.${domain_levels[-1]}" \
-    -H "X-Auth-Email: $(catapult company.cloudflare_email)" \
-    -H "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
-    -H "Content-Type: application/json")
+    cloudflare_zone=$(curl --silent --show-error --connect-timeout 5 --max-time 5 --write-out "HTTPSTATUS:%{http_code}" --request GET "https://api.cloudflare.com/client/v4/zones?name=${domain_levels[-2]}.${domain_levels[-1]}" \
+    --header "X-Auth-Email: $(catapult company.cloudflare_email)" \
+    --header "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
+    --header "Content-Type: application/json")
+    cloudflare_zone_status=$(echo "${cloudflare_zone}" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+    cloudflare_zone=$(echo "${cloudflare_zone}" | sed -e 's/HTTPSTATUS\:.*//g')
+
+    # check for a curl error
+    if [ $cloudflare_zone_status == 000 ]; then
+
+        echo "there was a problem with the cloudflare api request - please visit https://www.cloudflarestatus.com to see if there is a problem"
 
     # clear cloudflare zone cache by zone id, if it exists
-    if [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"]')" != "[]" ]; then
+    elif [ "$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"]')" != "[]" ]; then
+
         cloudflare_zone_id=$(echo "${cloudflare_zone}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["result"][0]["id"]')
-        cloudflare_zone_cache=$(curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/${cloudflare_zone_id}/purge_cache" \
-        -H "X-Auth-Email: $(catapult company.cloudflare_email)" \
-        -H "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
-        -H "Content-Type: application/json" \
+        cloudflare_zone_cache=$(curl --silent --show-error --connect-timeout 5 --max-time 5 --write-out "HTTPSTATUS:%{http_code}" --request DELETE "https://api.cloudflare.com/client/v4/zones/${cloudflare_zone_id}/purge_cache" \
+        --header "X-Auth-Email: $(catapult company.cloudflare_email)" \
+        --header "X-Auth-Key: $(catapult company.cloudflare_api_key)" \
+        --header "Content-Type: application/json" \
         --data "{\"purge_everything\":true}")
-        if [ "$(echo "${cloudflare_zone_cache}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["success"]')" == "False" ]; then
+        cloudflare_zone_cache_status=$(echo "${cloudflare_zone}" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+        cloudflare_zone_cache=$(echo "${cloudflare_zone}" | sed -e 's/HTTPSTATUS\:.*//g')
+
+        # check for a curl error
+        if [ $cloudflare_zone_status == 000 ]; then
+            echo "there was a problem with the cloudflare api request - please visit https://www.cloudflarestatus.com to see if there is a problem"
+        elif [ "$(echo "${cloudflare_zone_cache}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["success"]')" == "False" ]; then
             echo "[${domain_levels[-2]}.${domain_levels[-1]}] $(echo ${cloudflare_zone_cache} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["errors"][0]["message"]')"
         else
             echo "[${domain_levels[-2]}.${domain_levels[-1]}] successfully cleared cache"
         fi
+
     else
+
         echo "[${domain_levels[-2]}.${domain_levels[-1]}] cloudflare zone does not exist"
+        
     fi
 
 done
