e32d79f4af52ca7cca1e385a2d480d901085b2a0
Seth Reeser <seth.reeser@calciumusa.com>
2016-02-16 14:47:12 -0500

    RedHat: Re-introduce New Relic APM and Server to LocalDev for consistency.

--- provisioners/redhat/modules/newrelic_apm_php.sh
+++ provisioners/redhat/modules/newrelic_apm_php.sh
@@ -1,20 +1,16 @@
 source "/catapult/provisioners/redhat/modules/catapult.sh"
 
-if [ $1 = "dev" ]; then
-    echo -e "\t * skipping dev..."
-else
-    # add the new relic yum repository
-    rpm --hash --upgrade --verbose https://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm
+# add the new relic yum repository
+rpm --hash --upgrade --verbose https://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm
 
-    # install the apm php package
-    sudo yum install -y newrelic-php5
-    # set the apm php appname
-    sed -i -e "s#newrelic\.appname.*#newrelic.appname = \"$(catapult company.name)-${1}-redhat\"#g" "/etc/php.d/newrelic.ini"
-    # apm php installed but license key does not match
-    NR_INSTALL_SILENT="true", NR_INSTALL_KEY="$(catapult company.newrelic_license_key)" /usr/bin/newrelic-install install
-    # reload newrelic daemon
-    /etc/init.d/newrelic-daemon restart
-    # reload apache
-    sudo systemctl reload httpd.service
-    sudo systemctl status httpd.service
-fi
+# install the apm php package
+sudo yum install -y newrelic-php5
+# set the apm php appname
+sed -i -e "s#newrelic\.appname.*#newrelic.appname = \"$(catapult company.name)-${1}-redhat\"#g" "/etc/php.d/newrelic.ini"
+# apm php installed but license key does not match
+NR_INSTALL_SILENT="true", NR_INSTALL_KEY="$(catapult company.newrelic_license_key)" /usr/bin/newrelic-install install
+# reload newrelic daemon
+/etc/init.d/newrelic-daemon restart
+# reload apache
+sudo systemctl reload httpd.service
+sudo systemctl status httpd.service
--- provisioners/redhat/modules/newrelic_sysmon.sh
+++ provisioners/redhat/modules/newrelic_sysmon.sh
@@ -1,15 +1,11 @@
 source "/catapult/provisioners/redhat/modules/catapult.sh"
 
-if [ $1 = "dev" ]; then
-    echo -e "\t * skipping dev..."
-else
-    # add the new relic yum repository
-    rpm --hash --upgrade --verbose https://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm
+# add the new relic yum repository
+rpm --hash --upgrade --verbose https://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm
 
-    # install the server monitor package
-    sudo yum install -y newrelic-sysmond
-    # configure & start the server monitor daemon
-    nrsysmond-config --set license_key=$(catapult company.newrelic_license_key)
-    # start the server monitor daemon
-    /etc/init.d/newrelic-sysmond start
-fi
+# install the server monitor package
+sudo yum install -y newrelic-sysmond
+# configure & start the server monitor daemon
+nrsysmond-config --set license_key=$(catapult company.newrelic_license_key)
+# start the server monitor daemon
+/etc/init.d/newrelic-sysmond start
