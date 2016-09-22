1cff843c2820fdb4fc5ed6ef178b32b5b674fd9a
Seth Reeser <seth.reeser@devopsgroup.io>
2016-09-21 21:24:31 -0400

    RedHat: Explicitly disable SELinux for very bare boxes.

--- provisioners/redhat/modules/system.sh
+++ provisioners/redhat/modules/system.sh
@@ -46,6 +46,24 @@ fi
 
 
 
+echo -e "\n> system SELinux configuration"
+sudo cat > /etc/sysconfig/selinux << EOF
+# This file controls the state of SELinux on the system.
+# SELINUX= can take one of these three values:
+#     enforcing - SELinux security policy is enforced.
+#     permissive - SELinux prints warnings instead of enforcing.
+#     disabled - No SELinux policy is loaded.
+SELINUX=disabled
+# SELINUXTYPE= can take one of these two values:
+#     targeted - Targeted processes are protected,
+#     minimum - Modification of targeted policy. Only selected processes are protected. 
+#     mls - Multi Level Security protection.
+SELINUXTYPE=targeted
+EOF
+sestatus -v
+
+
+
 echo -e "\n> system swap configuration"
 # get current swaps
 swaps=$(swapon --noheadings --show=NAME)
