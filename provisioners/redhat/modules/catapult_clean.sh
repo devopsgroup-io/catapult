bdadc0b4d63f392762808eb962a1b6ea2b700713
Seth Reeser <seth.reeser@devopsgroup.io>
2016-10-20 12:57:30 -0400

    RedHat/Windows: Standardize secret decrypt and removal. Do not remove secrets in LocalDev, to allow for multiple machine provisioning. Platform: Rough draft of a Catapult platform topology.

--- provisioners/redhat/modules/catapult_clean.sh
+++
@@ -1,3 +0,0 @@
-sudo rm /catapult/secrets/configuration.yml
-sudo rm /catapult/secrets/id_rsa
-sudo rm /catapult/secrets/id_rsa.pub
--- provisioners/redhat/modules/catapult_decrypt.sh
+++
@@ -1,7 +0,0 @@
-gpg --verbose --batch --yes --passphrase ${3} --output /catapult/secrets/configuration.yml --decrypt /catapult/secrets/configuration.yml.gpg
-gpg --verbose --batch --yes --passphrase ${3} --output /catapult/secrets/id_rsa --decrypt /catapult/secrets/id_rsa.gpg
-gpg --verbose --batch --yes --passphrase ${3} --output /catapult/secrets/id_rsa.pub --decrypt /catapult/secrets/id_rsa.pub.gpg
-
-chmod 700 /catapult/secrets/configuration.yml
-chmod 700 /catapult/secrets/id_rsa
-chmod 700 /catapult/secrets/id_rsa.pub
