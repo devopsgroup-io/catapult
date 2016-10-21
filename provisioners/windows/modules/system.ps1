f156b5d42561a61472bc8124b66cb89f7240b6a0
Seth Reeser <seth.reeser@devopsgroup.io>
2016-10-20 20:17:19 -0400

    Windows: Introduce a correct method for installing MS SQL Server Express. Introduce credentials for mssql in configuration. Introduce Microsoft Update Service configuration.

--- provisioners/windows/modules/system.ps1
+++ provisioners/windows/modules/system.ps1
@@ -95,7 +95,7 @@ if (-not(test-path -path "c:\catapult\provisioners\windows\installers\temp\dotne
 }
 
 
-echo "`n=> Installing .NET 3.5 (This may take a while...)"
+echo "`n=> Installing .NET 3.5..."
 if (-not(test-path -path "c:\windows\Microsoft.NET\Framework64\v3.5\")) {
     Install-WindowsFeature Net-Framework-Core -source "c:\catapult\provisioners\windows\installers\temp\dotnetfx35.exe"
 } else {
@@ -115,7 +115,7 @@ if (-not(test-path -path "c:\catapult\provisioners\windows\installers\temp\dotNe
 }
 
 
-echo "`n=> Installing .NET 4.0 (This may take a while...)"
+echo "`n=> Installing .NET 4.0..."
 if (-not(test-path -path "c:\windows\Microsoft.NET\Framework64\v4.0.30319\")) {
     start-process -filepath "c:\catapult\provisioners\windows\installers\temp\dotNetFx40_Full_x86_x64.exe" -argumentlist "/q /norestart /log c:\catapult\provisioners\windows\logs\dotNetFx40_Full_x86_x64.exe.log" -Wait -RedirectStandardOutput $provision -RedirectStandardError $provisionError
     echo "Restarting Windows..."
@@ -134,7 +134,7 @@ Where { $_.PSChildName -match '^(?!S)\p{L}'} |
 Select PSChildName, Version, Release
 
 
-echo "`n=> Installing Web Platform Installer (This may take a while...)"
+echo "`n=> Installing Web Platform Installer..."
 # http://www.iis.net/learn/install/web-platform-installer/web-platform-installer-v4-command-line-webpicmdexe-rtw-release
 if (-not(test-path -path "c:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe")) {
     # https://github.com/fdcastel/psunattended/blob/master/PSUnattended.ps1
@@ -176,7 +176,7 @@ for ($i=0; $i -le 10; $i++) {
 }
 
 
-echo "`n=> Checking for Windows Updates (This may take a while...)"
+echo "`n=> Checking for Windows Updates (This may take a while)..."
 # configure windows update settings
 $windows_update_settings = (new-object -com "Microsoft.Update.AutoUpdate").Settings
 # 1 - Never check for updates
@@ -191,5 +191,19 @@ $windows_update_settings.NonAdministratorsElevated=$true
 $windows_update_settings.FeaturedUpdatesEnabled=$true
 $windows_update_settings.save()
 $windows_update_settings
+# ensure we're set to the Microsoft Update Service
+echo "Configuring the Microsoft Update Service..."
+# Examples Of ServiceID:
+# Windows Update                  9482f4b4-e343-43b6-b170-9a65bc822c77 
+# Microsoft Update                7971f918-a847-4430-9279-4a52d1efe18d 
+# Windows Store                   117cab2d-82b1-4b5a-a08c-4d62dbee7782 
+# Windows Server Update Service   3da21691-e39d-4da6-8a4b-b43877bcb1b7 
+# Performing the operation "Register Windows Update Service Manager: 7971f918-a847-4430-9279-4a52d1efe18d" on target "DEV-WIN".
+# [Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"): Y
+Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -confirm:$false
+# get registered update service
+echo "Getting the Microsoft Update Service..."
+Get-WUServiceManager
 # install latest updates
-Get-WUInstall -WindowsUpdate -AcceptAll -IgnoreReboot
+echo "Checking for Microsoft Updates..."
+Get-WUInstall -MicrosoftUpdate -AcceptAll -IgnoreReboot
