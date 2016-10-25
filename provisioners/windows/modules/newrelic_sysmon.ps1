1ea698dcb122db7a7cfa37ec4e3c76bb193a09b2
Seth Reeser <seth.reeser@devopsgroup.io>
2016-10-25 14:59:57 -0400

    Windows: Correctly accommodate first provision for git_clean and newrelic_sysmon.

--- provisioners/windows/modules/newrelic_sysmon.ps1
+++ provisioners/windows/modules/newrelic_sysmon.ps1
@@ -1,7 +1,7 @@
 . "c:\catapult\provisioners\windows\modules\catapult.ps1"
 
-$service = Get-Service -Name "nrsvrmon"
-if ($service.Status -ne "Running") {
+
+if (-not(Get-Service -Name "nrsvrmon" -ErrorAction SilentlyContinue)) {
     echo "New Relic Server Monitor Service is not running, installing..."
     # the unadvertised arg NR_HOST=<host name> is also available, retaining the windows hostname due to the 15 char limit
     msiexec /i "c:\catapult\provisioners\windows\installers\NewRelicServerMonitor_x64_3.3.6.0.msi" /L*v "c:\catapult\provisioners\windows\logs\NewRelicServerMonitor_x64_3.3.6.0.msi.log" /qn NR_LICENSE_KEY=$($configuration.company.newrelic_license_key)
