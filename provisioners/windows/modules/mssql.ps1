1f77b8f24b1c50680c94f9cffbc35f278318fb37
Seth Reeser <seth.reeser@devopsgroup.io>
2016-10-25 22:11:26 -0400

    Windows: Manage the mssql sa and environment account integrity, as well as, database roles.

--- provisioners/windows/modules/mssql.ps1
+++ provisioners/windows/modules/mssql.ps1
@@ -1,6 +1,11 @@
 . "c:\catapult\provisioners\windows\modules\catapult.ps1"
 
 
+$mssql_user = $configuration.environments.$($args[0]).servers.windows_mssql.mssql.user
+$mssql_user_password = $configuration.environments.$($args[0]).servers.windows_mssql.mssql.user.password
+$mssql_sa_password = $configuration.environments.$($args[0]).servers.windows_mssql.mssql.sa_password
+
+
 echo "`n=> Downloading SQL Server 2014 Express Edition (This may take a while)..."
 if (-not(test-path -path "c:\catapult\provisioners\windows\installers\temp\SQLEXPRWT_x64_ENU.exe")) {
     $url = "https://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/ExpressAndTools%2064BIT/SQLEXPRWT_x64_ENU.exe"
@@ -29,7 +34,7 @@ if (-not(test-path -path "c:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRE
     # https://msdn.microsoft.com/en-us/library/dd239405(v=sql.120).aspx
     # setup parameters
     # https://msdn.microsoft.com/en-us/library/ms144259(v=sql.120).aspx
-    start-process -filepath "c:\catapult\provisioners\windows\installers\temp\SQLEXPRWT_x64_ENU\setup.exe" -argumentlist ("/IACCEPTSQLSERVERLICENSETERMS /SQLSYSADMINACCOUNTS={1} /SAPWD={0} /SQLSVCPASSWORD={0} /AGTSVCPASSWORD={0} /ASSVCPASSWORD={0} /ISSVCPASSWORD={0} /RSSVCPASSWORD={0} /ConfigurationFile=c:\catapult\provisioners\windows\installers\MicrosoftSQLServer\ConfigurationFile.ini" -f $configuration.environments.$($args[0]).servers.windows_mssql.mssql.sa_password,"$($hostname)\$($env:username)") -Wait -RedirectStandardOutput $provision -RedirectStandardError $provisionError
+    start-process -filepath "c:\catapult\provisioners\windows\installers\temp\SQLEXPRWT_x64_ENU\setup.exe" -argumentlist ("/IACCEPTSQLSERVERLICENSETERMS /SQLSYSADMINACCOUNTS={1} /SAPWD={0} /SQLSVCPASSWORD={0} /AGTSVCPASSWORD={0} /ASSVCPASSWORD={0} /ISSVCPASSWORD={0} /RSSVCPASSWORD={0} /ConfigurationFile=c:\catapult\provisioners\windows\installers\MicrosoftSQLServer\ConfigurationFile.ini" -f $mssql_sa_password,"$($hostname)\$($env:username)") -Wait -RedirectStandardOutput $provision -RedirectStandardError $provisionError
     get-content $provision
     get-content $provisionError
     $directory_latest = Get-ChildItem -Path "c:\Program Files\Microsoft SQL Server\120\Setup Bootstrap\Log" | Sort-Object LastAccessTime -Descending | Select-Object -First 1
@@ -39,22 +44,60 @@ if (-not(test-path -path "c:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRE
     echo "- Mandatory 30 second post-install cool down period, please wait..."
     start-sleep -s 30
     start-service 'MSSQL$SQLEXPRESS'
-} else {
-    echo "- Installed, skipping..."
+    restart-service 'MSSQL$SQLEXPRESS' -Force
+ } else {
+     echo "- Installed, skipping..."
+ }
+ 
+ 
+echo "`n=> Managing SQL Server Logins..."
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+$server = new-object ("Microsoft.SqlServer.Management.Smo.Server") -ArgumentList "localhost\SQLEXPRESS"
+# manage the sa mssql user
+$login = $server.Logins | ? {$_.Name -eq "sa"}
+$login.ChangePassword($mssql_sa_password)
+$login.PasswordPolicyEnforced = 1
+$login.Alter()
+$login.Refresh()
+# manage the environment mssql user
+if ($server.Logins.name -notcontains $mssql_user) {
+    $login = new-object Microsoft.SqlServer.Management.Smo.Login($server, $mssql_user)
+    $login.LoginType = [Microsoft.SqlServer.Management.Smo.LoginType]::SqlLogin
+    $login.PasswordPolicyEnforced = $false
+    $login.PasswordExpirationEnabled = $false
+    $login.Create($mssql_user_password)
 }
-
+$login = $server.Logins | ? {$_.Name -eq $mssql_user}
+$login.ChangePassword($mssql_user_password)
+$login.PasswordPolicyEnforced = 1
+$login.Alter()
+$login.Refresh()
+ 
 
 echo "`n=> Managing SQL Server Databases..."
 [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
-$server = new-object ("Microsoft.SqlServer.Management.Smo.Server") -ArgumentList "localhost"
+$server = new-object ("Microsoft.SqlServer.Management.Smo.Server") -ArgumentList "localhost\SQLEXPRESS"
 foreach ($instance in $configuration.websites.iis) {
     $domainvaliddbname = ("{0}_{1}" -f $($args[0]), $instance.domain -replace "\.","_")
-    $database = New-Object Microsoft.SqlServer.Management.Smo.Database($server, $domainvaliddbname)
-    $database.Create()
-}
-
-foreach ($database in $server.databases) {
-    echo $database.name
+    # manage the database
+    if ($server.Databases.name -notcontains $domainvaliddbname) {
+        $database = new-object Microsoft.SqlServer.Management.Smo.Database($server, $domainvaliddbname)
+        $database.Create()
+    }
+    # manage the environment mssql user per database
+    $database = $server.Databases[$domainvaliddbname];
+    if ($database.Users.name -notcontains $mssql_user) {
+        # Add user to database
+        $user = New-Object ('Microsoft.SqlServer.Management.Smo.User') ($database, $mssql_user)
+        $user.Login = $mssql_user
+        $user.Create()
+    }
+    $role = $database.Roles["db_datareader"]
+    $role.AddMember($mssql_user)
+    $role.Alter()
+    $role = $database.Roles["db_datawriter"]
+    $role.AddMember($mssql_user)
+    $role.Alter()
 }
 
 
