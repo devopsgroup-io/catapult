62535ee613e28da08f1ad8cefa817fec3cf6ed4f
Seth Reeser <seth.reeser@devopsgroup.io>
2017-01-10 15:39:30 -0500

    RedHat: Provide validation that Bamboo has fully started.

--- provisioners/redhat/modules/bamboo.sh
+++ provisioners/redhat/modules/bamboo.sh
@@ -24,10 +24,15 @@ sed --in-place 's/port="8085"/port="80"/g' /usr/local/src/bamboo/atlassian-bambo
 # start bamboo
 bash /usr/local/src/bamboo/atlassian-bamboo-5.13.2/bin/start-bamboo.sh
 
+# sleep a few seconds to allow start-bamboo.sh to run
+sleep 5
 # confirm that bamboo has started, the first start can expect a 5-10 minute delay 
 response=0
 until [ $response -eq 200 ]; do
-    response=$(curl --connect-timeout 5 --head --output /dev/null --retry 5 --silent --write-out '%{http_code}\n' --location --url http://127.0.0.1)
-    echo "Waiting for Bamboo to start..."
+    response=$(curl --connect-timeout 30 --head --output /dev/null --retry 1 --silent --write-out '%{http_code}\n' --location --url http://127.0.0.1)
+    echo "$(date) waiting for Bamboo to start, checking every 30 seconds..."
 done
-echo "Bamboo successfully started."
+echo "Bamboo successfully started"
+
+# echo out configuration, which includes the IP address of the bamboo instance
+cat /usr/local/src/bamboo/atlassian-bamboo/bamboo.cfg.xml
