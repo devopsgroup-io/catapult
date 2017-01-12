4f26c68b37c4c67731245174ad5293b7b5f2dd9c
Seth Reeser <seth.reeser@devopsgroup.io>
2017-01-12 10:25:19 -0500

    RedHat: Correct file out of the init script and catching curl response of 000.

---
+++ provisioners/redhat/installers/bamboo/bamboo.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+### BEGIN INIT INFO
+# Provides: bamboo
+# Required-Start: $local_fs $remote_fs $network $time
+# Required-Stop: $local_fs $remote_fs $network $time
+# Should-Start: $syslog
+# Should-Stop: $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Atlassian Bamboo Server
+### END INIT INFO
+# INIT Script
+######################################
+
+# Define some variables
+# Name of app ( bamboo, Confluence, etc )
+APP=bamboo
+# Name of the user to run as
+USER=root
+# Location of application's bin directory
+BASE=/usr/local/src/bamboo/atlassian-bamboo-5.13.2
+
+case "$1" in
+  # Start command
+  start)
+    echo "Starting $APP"
+    /bin/su - $USER -c "export BAMBOO_HOME=${BAMBOO_HOME}; $BASE/bin/start-bamboo.sh &> /dev/null"
+    ;;
+  # Stop command
+  stop)
+    echo "Stopping $APP"
+    /bin/su - $USER -c "$BASE/bin/stop-bamboo.sh &> /dev/null"
+    echo "$APP stopped successfully"
+    ;;
+   # Restart command
+   restart)
+        $0 stop
+        sleep 5
+        $0 start
+        ;;
+  *)
+    echo "Usage: /etc/init.d/$APP {start|restart|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
