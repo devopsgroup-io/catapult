ae0b5a692d2de5456a3ed37de62e1425648af07e
Seth Reeser <seth.reeser@devopsgroup.io>
2017-01-18 15:28:51 -0500

    RedHat: Split out the mysql module into 3; mysql, mysql_database_backup, and mysql_database_restore.

---
+++ provisioners/redhat/modules/mysql_database_restore.sh
@@ -0,0 +1,131 @@
+source "/catapult/provisioners/redhat/modules/catapult.sh"
+
+# set a variable to the .cnf
+dbconf="/catapult/provisioners/redhat/installers/temp/${1}.cnf"
+
+domain=$(catapult websites.apache.$5.domain)
+domain_tld_override=$(catapult websites.apache.$5.domain_tld_override)
+domainvaliddbname=$(catapult websites.apache.$5.domain | tr "." "_" | tr "-" "_")
+software=$(catapult websites.apache.$5.software)
+software_dbprefix=$(catapult websites.apache.$5.software_dbprefix)
+software_workflow=$(catapult websites.apache.$5.software_workflow)
+software_db=$(mysql --defaults-extra-file=$dbconf --silent --skip-column-names --execute "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${1}_${domainvaliddbname}'")
+software_db_tables=$(mysql --defaults-extra-file=$dbconf --silent --skip-column-names --execute "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '${1}_${domainvaliddbname}'")
+softwareroot=$(provisioners software.apache.${software}.softwareroot)
+webroot=$(catapult websites.apache.$5.webroot)
+
+
+# respect software_workflow
+if ([ test -n "${software}" ] && [ "${1}" = "production" ] && [ "${software_workflow}" = "downstream" ] && [ "${software_db}" != "" ] && [ "${software_db_tables}" != "0" ]) || ([ test -n "${software}" ] && [ "${1}" = "test" ] && [ "${software_workflow}" = "upstream" ] && [ "${software_db}" != "" ] && [ "${software_db_tables}" != "0" ]); then
+    : #no-op
+else
+    if [ -z "${software_db}" ]; then
+        echo -e "\t* workflow is set to ${software_workflow} and this is the ${1} environment, however, the database does not exist. performing a database restore..."
+    elif [ -z "${software_db_tables}" ]; then
+        echo -e "\t* workflow is set to ${software_workflow} and this is the ${1} environment, however, the database exists but contains no tables. performing a database restore..."
+    else
+        echo -e "\t* workflow is set to ${software_workflow} and this is the ${1} environment, performing a database restore..."
+    fi
+    # drop the database
+    # the loop is necessary just in case the database doesn't yet exist
+    for database in $(mysql --defaults-extra-file=$dbconf -e "show databases" | egrep -v "Database|mysql|information_schema|performance_schema"); do
+        if [ ${database} = ${1}_${domainvaliddbname} ]; then
+            mysql --defaults-extra-file=$dbconf -e "DROP DATABASE ${1}_${domainvaliddbname}";
+        fi
+    done
+    # create database
+    mysql --defaults-extra-file=$dbconf -e "CREATE DATABASE ${1}_${domainvaliddbname}"
+    # confirm we have a usable database backup
+    if ! [ -d "/var/www/repositories/apache/${domain}/_sql" ]; then
+        echo -e "\t* ~/_sql directory does not exist, ${software} may not function properly"
+    else
+        echo -e "\t* ~/_sql directory exists, looking for a valid database dump to restore from"
+        filenewest=$(ls "/var/www/repositories/apache/${domain}/_sql" | grep -E ^[0-9]{8}\.sql$ | sort --numeric-sort | tail -1)
+        for file in /var/www/repositories/apache/${domain}/_sql/*.*; do
+            if [[ "${file}" != *.sql ]]; then
+                echo -e "\t\t[invalid] [ ].sql [ ]YYYYMMDD.sql [ ]newest => $file"
+            elif ! [[ "$(basename "${file}")" =~ ^[0-9]{8}.sql$ ]]; then
+                echo -e "\t\t[invalid] [x].sql [ ]YYYYMMDD.sql [ ]newest => $file"
+            elif [[ "$(basename "$file")" != "${filenewest}" ]]; then
+                echo -e "\t\t[invalid] [x].sql [x]YYYYMMDD.sql [ ]newest => $file"
+            else
+                echo -e "\t\t[valid]   [x].sql [x]YYYYMMDD.sql [x]newest => $file"
+                echo -e "\t\t\trestoring..."
+                # support domain_tld_override for URL replacements
+                if [ -z "${domain_tld_override}" ]; then
+                    # create replace string and make sure to escape periods
+                    domain_url_replace=$(echo -e "${domain}" | sed 's/\./\\./g')
+                    # create string of final url
+                    if [ "${1}" = "production" ]; then
+                        domain_url="${domain}"
+                    else
+                        domain_url="${1}.${domain}"
+                    fi
+                else
+                    # create replace string and make sure to escape periods
+                    domain_url_replace=$(echo -e "${domain}.${domain_tld_override}|${domain}" | sed 's/\./\\./g')
+                    # create string of final url
+                    if [ "${1}" = "production" ]; then
+                        domain_url="${domain}.${domain_tld_override}"
+                    else
+                        domain_url="${1}.${domain}.${domain_tld_override}"
+                    fi
+                fi
+                # replace variances of the following urls during a restore to match the environment
+                # pay attention to the order of the (${domain}.${domain_tld_override|${domain}}) rule
+                # https://regex101.com/r/vF7hY9/2
+                # :\/\/(www\.)?(dev\.|test\.|qc\.)?(devopsgroup\.io\.example.com|devopsgroup\.io)
+                # ://dev.devopsgroup.io
+                # ://www.dev.devopsgroup.io
+                # ://test.devopsgroup.io
+                # ://www.test.devopsgroup.io
+                # ://devopsgroup.io
+                # ://www.devopsgroup.io
+                # ://dev.devopsgroup.io.example.com
+                # ://www.dev.devopsgroup.io.example.com
+                # ://test.devopsgroup.io.example.com
+                # ://www.test.devopsgroup.io.example.com
+                # ://devopsgroup.io.example.com
+                # ://www.devopsgroup.io.example.com
+
+                # pre-process database sql file
+                # for software without a cli tool for database url reference replacements, use sed to pre-process sql file and replace url references
+                if ([ "${software}" = "codeigniter2" ] \
+                 || [ "${software}" = "codeigniter3" ] \
+                 || [ "${software}" = "drupal6" ] \
+                 || [ "${software}" = "drupal7" ] \
+                 || [ "${software}" = "elgg1" ] \
+                 || [ "${software}" = "expressionengine3" ] \
+                 || [ "${software}" = "joomla3" ] \
+                 || [ "${software}" = "laravel5" ] \
+                 || [ "${software}" = "mediawiki1" ] \
+                 || [ "${software}" = "moodle3" ] \
+                 || [ "${software}" = "silverstripe3" ] \
+                 || [ "${software}" = "suitecrm7" ] \
+                 || [ "${software}" = "xenforo" ] \
+                 || [ "${software}" = "zendframework2" ]); then
+                    echo -e "\t* replacing URLs in the database to align with the enivronment..."
+                    replacements=$(grep --extended-regexp --only-matching --regexp=":\/\/(www\.)?(dev\.|test\.|qc\.)?(${domain_url_replace})" "/var/www/repositories/apache/${domain}/_sql/$(basename "$file")" | wc --lines)
+                    sed --regexp-extended --expression="s/:\/\/(www\.)?(dev\.|test\.|qc\.)?(${domain_url_replace})/:\/\/\1${domain_url}/g" "/var/www/repositories/apache/${domain}/_sql/$(basename "$file")" > "/var/www/repositories/apache/${domain}/_sql/${1}.$(basename "$file")"
+                    echo -e "\t* found and replaced ${replacements} occurrences"
+                else
+                    cp "/var/www/repositories/apache/${domain}/_sql/$(basename "$file")" "/var/www/repositories/apache/${domain}/_sql/${1}.$(basename "$file")"
+                fi
+
+                # restore the database sql file
+                mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} < "/var/www/repositories/apache/${domain}/_sql/${1}.$(basename "$file")"
+                rm --force "/var/www/repositories/apache/${domain}/_sql/${1}.$(basename "$file")"
+
+                # post-process database
+                # for software with a cli tool for database url reference replacements, use cli tool to post-process database and replace url references
+                if [[ "${software}" = "wordpress" ]]; then
+                    echo -e "\t* replacing URLs in the database to align with the enivronment..."
+                    wp-cli --allow-root --path="/var/www/repositories/apache/${domain}/${webroot}" search-replace ":\/\/(www\.)?(dev\.|test\.|qc\.)?(${domain_url_replace})" "://\$1${domain_url}" --regex | sed "s/^/\t\t/"
+                    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "UPDATE ${software_dbprefix}options SET option_value='$(catapult company.email)' WHERE option_name = 'admin_email';"
+                fi
+            fi
+        done
+    fi
+fi
+
+touch "/catapult/provisioners/redhat/logs/mysql_database_restore.$(catapult websites.apache.$5.domain).complete"
--- provisioners/redhat/modules/software_operations_meta.sh
+++ provisioners/redhat/modules/software_operations_meta.sh
@@ -8,8 +8,115 @@ webroot=$(catapult websites.apache.$5.webroot)
 
 softwareroot=$(provisioners software.apache.${software}.softwareroot)
 
+
+# reset admin credentials
+if [ "${software}" = "drupal6" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users (uid, pass, mail, status)
+        VALUES ('1', MD5('$(catapult environments.${1}.software.drupal.admin_password)'), '$(catapult company.email)', '1')
+        ON DUPLICATE KEY UPDATE name='admin', mail='$(catapult company.email)', pass=MD5('$(catapult environments.${1}.software.drupal.admin_password)'), status='1';
+    "
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users_roles (uid, rid)
+        VALUES ('1', '3')
+        ON DUPLICATE KEY UPDATE rid='3';
+    "
+
+elif [ "${software}" = "drupal7" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    password_hash=$(cd "/var/www/repositories/apache/${domain}/${webroot}" && php ./scripts/password-hash.sh $(catapult environments.${1}.software.drupal.admin_password))
+    password_hash=$(echo "${password_hash}" | awk '{ print $4 }' | tr -d " " | tr -d "\n")
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users (uid, pass, mail, status)
+        VALUES ('1', '${password_hash}', '$(catapult company.email)', '1')
+        ON DUPLICATE KEY UPDATE name='admin', mail='$(catapult company.email)', pass='${password_hash}', status='1';
+    "
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users_roles (uid, rid)
+        VALUES ('1', '3')
+        ON DUPLICATE KEY UPDATE rid='3';
+    "
+
+elif [ "${software}" = "elgg1" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users_entity (username, password_hash, email, banned, admin)
+        VALUES ('admin', MD5('$(catapult environments.${1}.software.admin_password)'), '$(catapult company.email)', 'no', 'yes')
+        ON DUPLICATE KEY UPDATE username='admin', password_hash=MD5('$(catapult environments.${1}.software.admin_password)'), email='$(catapult company.email)', banned='no', admin='yes';
+    "
+
+elif [ "${software}" = "joomla3" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        UPDATE ${software_dbprefix}users
+        SET username='admin', email='$(catapult company.email)', password=MD5('$(catapult environments.${1}.software.admin_password)'), block='0'
+        WHERE name='Super User';
+    "
+
+elif [ "${software}" = "mediawiki1" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}user (user_id, user_name, user_email)
+        VALUES ('1', 'Admin', '$(catapult company.email)')
+        ON DUPLICATE KEY UPDATE user_name='Admin', user_email='$(catapult company.email)';
+    "
+    cd "/var/www/repositories/apache/${domain}/${webroot}" && php maintenance/changePassword.php --userid="1" --password="$(catapult environments.${1}.software.admin_password)"
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}user_groups (ug_user, ug_group)
+        VALUES ('1', 'sysop')
+        ON DUPLICATE KEY UPDATE ug_user=ug_user, ug_group=ug_group;
+    "
+
+elif [ "${software}" = "moodle3" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        UPDATE ${software_dbprefix}user
+        SET username='admin', password=MD5('$(catapult environments.${1}.software.admin_password)'), suspended='0', email='$(catapult company.email)'
+        WHERE id='2';
+    "
+
+elif [ "${software}" = "silverstripe3" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        UPDATE ${software_dbprefix}Member
+        SET FirstName='Default Admin', Email='$(catapult company.email)', Password='$(catapult environments.${1}.software.admin_password)', PasswordEncryption='none', LockedOutUntil='NULL'
+        WHERE ID='1';
+    "
+    # a hack to encrypt the plain text password that we just set, wahoo!
+    cd "/var/www/repositories/apache/${domain}/${webroot}" && php framework/cli-script.php dev/tasks/EncryptAllPasswordsTask
+
+elif [ "${software}" = "suitecrm7" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users (id, user_name, user_hash, is_admin)
+        VALUES ('1', 'admin', MD5('$(catapult environments.${1}.software.admin_password)'), '1')
+        ON DUPLICATE KEY UPDATE user_name='admin', user_hash=MD5('$(catapult environments.${1}.software.admin_password)'), is_admin='1';
+    "
+
+elif [ "${software}" = "wordpress" ]; then
+
+    echo -e "\t* resetting ${software} admin password..."
+    mysql --defaults-extra-file=$dbconf ${1}_${domainvaliddbname} -e "
+        INSERT INTO ${software_dbprefix}users (id, user_login, user_pass, user_nicename, user_email, user_status, display_name)
+        VALUES ('1', 'admin', MD5('$(catapult environments.${1}.software.wordpress.admin_password)'), 'admin', '$(catapult company.email)', '0', 'admin')
+        ON DUPLICATE KEY UPDATE user_login='admin', user_pass=MD5('$(catapult environments.${1}.software.wordpress.admin_password)'), user_nicename='admin', user_email='$(catapult company.email)', user_status='0', display_name='admin';
+    "
+    wp-cli --allow-root --path="/var/www/repositories/apache/${domain}/${webroot}" user add-role 1 administrator
+
+fi
+
+
 # run software database operations
-if [ "$software" = "codeigniter2" ]; then
+if [ "${software}" = "codeigniter2" ]; then
 
     result=$(cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php index.php migrate)
     if echo $result | grep --extended-regexp --quiet --regexp="<html" --regexp="<\?"; then
@@ -18,7 +125,7 @@ if [ "$software" = "codeigniter2" ]; then
         echo $result
     fi
 
-elif [ "$software" = "codeigniter3" ]; then
+elif [ "${software}" = "codeigniter3" ]; then
     result=$(cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php index.php migrate)
     if echo $result | grep --extended-regexp --quiet --regexp="<html" --regexp="<\?"; then
         echo -e "Migrations are not configured"
@@ -26,36 +133,36 @@ elif [ "$software" = "codeigniter3" ]; then
         echo $result
     fi
 
-elif [ "$software" = "drupal6" ]; then
+elif [ "${software}" = "drupal6" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y watchdog-delete all
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y core-cron
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y updatedb
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y cache-clear all
 
-elif [ "$software" = "drupal7" ]; then
+elif [ "${software}" = "drupal7" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y watchdog-delete all
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y core-cron
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y updatedb
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && drush -y cache-clear all
 
-elif [ "$software" = "elgg1" ]; then
+elif [ "${software}" = "elgg1" ]; then
 
     echo "nothing to perform, skipping..."
 
-elif [ "$software" = "expressionengine3" ]; then
+elif [ "${software}" = "expressionengine3" ]; then
 
     echo "nothing to perform, skipping..."
 
-elif [ "$software" = "joomla3" ]; then
+elif [ "${software}" = "joomla3" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php cli/garbagecron.php
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php cli/update_cron.php
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php cli/finder_indexer.php
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php cli/deletefiles.php
 
-elif [ "$software" = "laravel5" ]; then
+elif [ "${software}" = "laravel5" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php artisan key:generate
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php artisan migrate
@@ -63,36 +170,36 @@ elif [ "$software" = "laravel5" ]; then
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php artisan clear-compiled
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php artisan optimize
 
-elif [ "$software" = "mediawiki1" ]; then
+elif [ "${software}" = "mediawiki1" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php maintenance/update.php
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php maintenance/runJobs.php
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php maintenance/rebuildall.php
 
-elif [ "$software" = "moodle3" ]; then
+elif [ "${software}" = "moodle3" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php admin/cli/cron.php
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php admin/cli/purge_caches.php
 
-elif [ "$software" = "silverstripe3" ]; then
+elif [ "${software}" = "silverstripe3" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php framework/cli-script.php dev/tasks/MigrationTask
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php framework/cli-script.php dev/build "flush=1"
 
-elif [ "$software" = "suitecrm7" ]; then
+elif [ "${software}" = "suitecrm7" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && php cron.php
 
-elif [ "$software" = "wordpress" ]; then
+elif [ "${software}" = "wordpress" ]; then
 
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli --allow-root core update-db
     cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli --allow-root cache flush
 
-elif [ "$software" = "xenforo" ]; then
+elif [ "${software}" = "xenforo" ]; then
 
     echo "nothing to perform, skipping..."
 
-elif [ "$software" = "zendframework2" ]; then
+elif [ "${software}" = "zendframework2" ]; then
 
     echo "nothing to perform, skipping..."
 
