5fdfa46e6c0bd31dbed0fda9ee03d5277bb483e6
Seth Reeser <seth.reeser@devopsgroup.io>
2017-03-19 14:21:59 -0400

    Build: Upgrade build server to Bamboo v5.15.3

--- provisioners/redhat/installers/bamboo/bamboo.sh
+++ provisioners/redhat/installers/bamboo/bamboo.sh
@@ -19,7 +19,7 @@ APP=bamboo
 # Name of the user to run as
 USER=root
 # Location of application's bin directory
-BASE=/usr/local/src/bamboo/atlassian-bamboo-5.13.2
+BASE=/usr/local/src/bamboo/atlassian-bamboo-5.15.3
 
 case "$1" in
   # Start command
