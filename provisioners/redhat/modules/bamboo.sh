6c530a76b8a6273ef92a7fe66d28a24f426b95ca
Seth Reeser <seth.reeser@devopsgroup.io>
2017-03-19 12:40:14 -0400

    Build: Upgrade build server to Bamboo v5.15.3

--- provisioners/redhat/modules/bamboo.sh
+++ provisioners/redhat/modules/bamboo.sh
@@ -1,25 +1,25 @@
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64 
 java -version
 
-if [ ! -d /usr/local/src/bamboo/atlassian-bamboo-5.13.2/atlassian-bamboo ]; then
+if [ ! -d /usr/local/src/bamboo/atlassian-bamboo-5.15.3/atlassian-bamboo ]; then
 
     mkdir --parents /usr/local/src/bamboo
     cd /usr/local/src/bamboo
     
-    curl --silent --show-error --connect-timeout 5 --output bamboo.tar.gz --retry 5 --location --url https://www.atlassian.com/software/bamboo/downloads/binary/atlassian-bamboo-5.13.2.tar.gz
+    curl --silent --show-error --connect-timeout 5 --output bamboo.tar.gz --retry 5 --location --url https://www.atlassian.com/software/bamboo/downloads/binary/atlassian-bamboo-5.15.3.tar.gz
 
     tar -xzf bamboo.tar.gz
 
 fi
 
 mkdir --parents /usr/local/src/bamboo/atlassian-bamboo
-sudo cat > "/usr/local/src/bamboo/atlassian-bamboo-5.13.2/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties" << EOF
+sudo cat > "/usr/local/src/bamboo/atlassian-bamboo-5.15.3/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties" << EOF
     bamboo.home=/usr/local/src/bamboo/atlassian-bamboo
 EOF
 
 # run bamboo as port 80
 sed --in-place 's/:8085//g' /usr/local/src/bamboo/atlassian-bamboo/xml-data/configuration/administration.xml
-sed --in-place 's/port="8085"/port="80"/g' /usr/local/src/bamboo/atlassian-bamboo-5.13.2/conf/server.xml
+sed --in-place 's/port="8085"/port="80"/g' /usr/local/src/bamboo/atlassian-bamboo-5.15.3/conf/server.xml
 
 # run bamboo as as service
 # https://confluence.atlassian.com/bamboo/running-bamboo-as-a-linux-service-416056046.html
@@ -31,7 +31,7 @@ sudo /sbin/chkconfig --add bamboo
 # enable bamboo on startup
 sudo systemctl enable bamboo
 # start bamboo
-#bash /usr/local/src/bamboo/atlassian-bamboo-5.13.2/bin/start-bamboo.sh
+#bash /usr/local/src/bamboo/atlassian-bamboo-5.15.3/bin/start-bamboo.sh
 sudo systemctl start bamboo
 
 # sleep a few seconds to allow start-bamboo.sh to start
