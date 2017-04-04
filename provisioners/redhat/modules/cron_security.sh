899420a1032a27c212ba34e3faae1ab4a7091f95
Seth Reeser <seth.reeser@devopsgroup.io>
2017-04-04 14:48:03 -0400

    RedHat: Polish the output of the cron_security module.

--- provisioners/redhat/modules/cron_security.sh
+++ provisioners/redhat/modules/cron_security.sh
@@ -4,19 +4,20 @@
 
 /bin/echo -e "\nhere are recent ip addresses blocked by fail2ban"
 /bin/echo -e "================================================"
-/bin/tail /var/log/fail2ban.log | grep "Ban"
+/bin/grep "Ban" /var/log/fail2ban.log* | /bin/head -n 20
 
 /bin/echo -e "\nhere are recent 404s targeted at this server's ip address"
 /bin/echo -e "========================================================="
-/bin/grep --extended-regexp --max-count=10 --regexp="\" 404 " /var/log/httpd/access_log
+/bin/grep --extended-regexp --max-count=20 --regexp="\" 404 " /var/log/httpd/access_log
 
 /bin/echo -e "\nhere are recent 404s potentially targeted at vhosts"
 /bin/echo -e "==================================================="
-/bin/grep --extended-regexp --max-count=10 --regexp="\" 404 " /var/log/httpd/*/access_log
+/bin/grep --extended-regexp --max-count=20 --regexp="\" 404 " /var/log/httpd/*/access_log
 
 /bin/echo -e "\nhere are recent matches from a keyword threatlist potentially targeted at vhosts"
+/bin/echo -e "(denied|failed|failure|invalid|limit|permission)"
 /bin/echo -e "================================================================================"
-/bin/grep --extended-regexp --max-count=10 --regexp="(denied|failed|failure|invalid|limit|permission)" /var/log/httpd/*/error_log
+/bin/grep --extended-regexp --max-count=20 --regexp="(denied|failed|failure|invalid|limit|permission)" /var/log/httpd/*/error_log
 
 /bin/echo -e "\nhere are the currently configured fail2ban jails"
 /bin/echo -e "================================================"
