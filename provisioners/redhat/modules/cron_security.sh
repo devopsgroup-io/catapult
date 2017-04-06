66c170ad79e2bc929238c795f06d806d19869b39
Seth Reeser <seth.reeser@devopsgroup.io>
2017-04-06 12:04:07 -0400

    RedHat: Accommodate servers not running httpd.

--- provisioners/redhat/modules/cron_security.sh
+++ provisioners/redhat/modules/cron_security.sh
@@ -6,18 +6,22 @@
 /bin/echo -e "================================================"
 /bin/grep "Ban" /var/log/fail2ban.log* | /bin/head -n 20
 
-/bin/echo -e "\nhere are recent 404s targeted at this server's ip address"
-/bin/echo -e "========================================================="
-/bin/grep --extended-regexp --max-count=20 --regexp="\" 404 " /var/log/httpd/access_log
+if [ -d "/var/log/httpd" ]; then
 
-/bin/echo -e "\nhere are recent 404s potentially targeted at vhosts"
-/bin/echo -e "==================================================="
-/bin/grep --extended-regexp --max-count=20 --regexp="\" 404 " /var/log/httpd/*/access_log
+    /bin/echo -e "\nhere are recent 404s targeted at this server's ip address"
+    /bin/echo -e "========================================================="
+    /bin/grep --extended-regexp --max-count=20 --regexp="\" 404 " /var/log/httpd/access_log
 
-/bin/echo -e "\nhere are recent matches from a keyword threatlist potentially targeted at vhosts"
-/bin/echo -e "(denied|failed|failure|invalid|limit|permission)"
-/bin/echo -e "================================================================================"
-/bin/grep --extended-regexp --max-count=20 --regexp="(denied|failed|failure|invalid|limit|permission)" /var/log/httpd/*/error_log
+    /bin/echo -e "\nhere are recent 404s potentially targeted at vhosts"
+    /bin/echo -e "==================================================="
+    /bin/grep --extended-regexp --max-count=20 --regexp="\" 404 " /var/log/httpd/*/access_log
+
+    /bin/echo -e "\nhere are recent matches from a keyword threatlist potentially targeted at vhosts"
+    /bin/echo -e "(denied|failed|failure|invalid|limit|permission)"
+    /bin/echo -e "================================================================================"
+    /bin/grep --extended-regexp --max-count=20 --regexp="(denied|failed|failure|invalid|limit|permission)" /var/log/httpd/*/error_log
+
+fi
 
 /bin/echo -e "\nhere are the currently configured fail2ban jails"
 /bin/echo -e "================================================"
