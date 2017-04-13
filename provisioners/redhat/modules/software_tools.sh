6df40904e45adc5dcab27ec098209e1f64500c16
Seth Reeser <seth.reeser@devopsgroup.io>
2017-04-13 10:33:37 -0400

    RedHat: Update Drush version.

--- provisioners/redhat/modules/software_tools.sh
+++ provisioners/redhat/modules/software_tools.sh
@@ -35,7 +35,7 @@ fi
 # update to specific drush version
 cd /usr/local/src/drush \
     && git fetch \
-    && git checkout --force 8.1.7 \
+    && git checkout --force 8.1.10 \
     && composer install
 drush --version
 
