1138d3fad61396161c5b9ffb1c9a7c8d5bb22a1d
Seth Reeser <seth.reeser@devopsgroup.io>
2017-04-27 17:26:03 -0400

    Windows: Introduction of automated deployments via Bamboo for Windows.

---
+++ provisioners/windows/provision.py
@@ -0,0 +1,9 @@
+import sys
+import winrm
+
+winrmsession = winrm.Session(str(sys.argv[1]),auth=(str(sys.argv[2]),str(sys.argv[3])))
+winrmsession.run_ps("Set-ExecutionPolicy RemoteSigned")
+result = winrmsession.run_ps( 'powershell -file c:\catapult\provisioners\windows\provision.ps1' + ' "' + str(sys.argv[4]) + '" "' + str(sys.argv[5]) + '" "' + str(sys.argv[6]) + '" "' + str(sys.argv[7]) + '"' )
+
+print result.std_out
+print result.std_err
