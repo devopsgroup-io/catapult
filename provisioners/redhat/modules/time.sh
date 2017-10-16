2dc9b85c512a94c020ce1201dd260f30a76a67c8
Seth Reeser <seth.reeser@devopsgroup.io>
2017-10-16 17:23:32 -0400

    RedHat: Force time update and differentiate system and hardware clock.

--- provisioners/redhat/modules/time.sh
+++ provisioners/redhat/modules/time.sh
@@ -2,16 +2,26 @@ source "/catapult/provisioners/redhat/modules/catapult.sh"
 
 # set timezone
 sudo timedatectl set-timezone "$(echo "${configuration}" | shyaml get-value company.timezone_redhat)"
+# configure the realtime clock
+sudo timedatectl --adjust-system-clock set-local-rtc false
 # install ntp
 sudo yum install -y ntp
 sudo systemctl enable ntpd.service
 sudo systemctl start ntpd.service
 
+echo "> synchronize the hardware clock"
+sudo systemctl stop ntpd.service
+sudo ntpd -qg
+sudo systemctl start ntpd.service
+
+echo "> current hardware clock datetime"
+sudo hwclock --debug
+
 echo "> ntp peers"
-ntpq -p
+sudo ntpq -p
 
 echo "> ntp partner"
-ntpstat
+sudo ntpstat
 
-echo "> current datetime"
-timedatectl status
+echo "> current system datetime"
+sudo timedatectl status
