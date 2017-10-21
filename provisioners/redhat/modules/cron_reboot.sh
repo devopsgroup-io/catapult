1d329d8b136a6c893bd0c11aeb5149afe78ef621
Seth Reeser <seth.reeser@devopsgroup.io>
2017-10-21 13:36:23 -0400

    RedHat: Reboot only at 3:05 am.

--- provisioners/redhat/modules/cron_reboot.sh
+++ provisioners/redhat/modules/cron_reboot.sh
@@ -4,5 +4,5 @@ kernel_running=$(/bin/uname --release)
 kernel_running="kernel-${kernel_running}"
 kernel_staged=$(/bin/rpm --last --query kernel | /bin/head --lines 1 | /bin/awk '{print $1}')
 if [ "${kernel_running}" != "${kernel_staged}" ]; then
-    /sbin/reboot
+    /sbin/shutdown --reboot 03:05
 fi
