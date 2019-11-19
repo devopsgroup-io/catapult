72922cc1ccc2b1f478e43f4578027c22c7f6ea65
Seth Reeser <seth.reeser@devopsgroup.io>
2019-11-19 13:10:14 -0500

    RedHat: Update to latest website software and software tool versions.

--- provisioners/redhat/modules/software_tools.sh
+++ provisioners/redhat/modules/software_tools.sh
@@ -44,7 +44,7 @@ fi
 # update to specific drush version
 cd /usr/local/src/drush \
     && git fetch \
-    && git checkout --force 8.1.18 \
+    && git checkout --force 8.3.0 \
     && composer install
 drush --version
 
