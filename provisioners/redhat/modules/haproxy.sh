edb3a7365f8879d02c7fc7727cd9898d042e14f0
Seth Reeser <seth.reeser@devopsgroup.io>
2020-02-13 13:12:13 -0500

    RedHat: Migrate from a layer 4 to layer 7 load balancer and move HTTPS certificate termination to HAProxy.

--- provisioners/redhat/modules/haproxy.sh
+++ provisioners/redhat/modules/haproxy.sh
@@ -22,8 +22,8 @@ backend backend_http
 backend backend_https
     balance source
     mode http
-    server redhat 127.0.0.1:8081 check ssl verify none
-    server redhat1 ${redhat1_ip}:8081 check ssl verify none
+    server redhat 127.0.0.1:8080 check
+    server redhat1 ${redhat1_ip}:8080 check
 
 EOF
 else
@@ -36,7 +36,7 @@ backend backend_http
 backend backend_https
     balance source
     mode http
-    server redhat 127.0.0.1:8081 check ssl verify none
+    server redhat 127.0.0.1:8080 check
 
 EOF
 fi
