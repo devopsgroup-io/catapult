96bc659c7503045fc700a22fbc935f5f03ddd7ce
Seth Reeser <seth.reeser@devopsgroup.io>
2022-08-07 11:43:59 -0400

    RedHat: Adding support for WordPress 6.  Adjust how wp-cli handles versions and core updates.

--- provisioners/redhat/modules/software_operations_file.sh
+++ provisioners/redhat/modules/software_operations_file.sh
@@ -381,7 +381,7 @@ if hash composer 2>/dev/null && hash drush 2>/dev/null && hash wp-cli 2>/dev/nul
             if [ "${software_auto_update}" = "true" ]; then
                 cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root theme update --all
                 cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root plugin update --all
-                cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root core update --version=6.9
+                cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root core update
                 cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root plugin update --all
                 cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root theme update --all
                 cd "/var/www/repositories/apache/${domain}/${webroot}${softwareroot}" && wp-cli-php72 --allow-root plugin install w3-total-cache
